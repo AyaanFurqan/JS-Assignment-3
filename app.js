1cd0f8e4e8acf9da3119dee92c896c95eae3672f
Ayaan <ayaanfurqan008@gmail.com>
2025-02-08 02:45:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+let city = prompt ("Enter Your city")
+if (city == "Karachi"){
+    console.log("Welcome to The City Of light")
+} else {
+    console.log("Karachi Ajao")
+}
+
+let gender = prompt ("Your Gender")
+if(gender == "Male"){
+    console.log("Kesa Hay")
+        
+} else{
+    console.log("Assalamwalikum")
+}
+if(gender == "Female"){
+    console.log("Good Morning Madam")
+} 
+else{
+    console.log("")
+}
+
+let trafficlight = prompt ("Enter Colour Of Traffic Light 'anyone colour'")
+
+
+
+if(trafficlight == "Red"){
+    console.log("Must Stop")
+}
+if(trafficlight == "Yellow"){
+    console.log("Ready to Go")
+}
+if(trafficlight == "Green"){
+    console.log("Move Now")
+}
+
+let Fuel = prompt ("How Much Fuel is in your car")
+if(Fuel => 0.25){
+    console.log("Please refill the fuel in your car")
+}
+
+let result = prompt ("Enter Your Percentage")
+if (result => 80){
+    console.log (Grade = "A-one")
+    console.log (Remarks = "Excellent")
+}
+else if (result => 70){
+    console.log (Grade = "A")
+    console.log (Remarks = "Good")
+}
+else if (result => 60)
+    console.log (Grade = "B")
+    console.log (Remarks = "You Need To Improve")
+
+// else if (result <= 60){
+//     console.log (Grade = "Fail")
+//     console.log (Remarks = "Sorry")
+// }
+
+let numgame = prompt ("Guess Number Between 1-10")
+if (numgame === "8"){
+    console.log ("Bingo! You Guessed Right")
+}  else{
+    console.log("You Guessed It Wrong")
+}
+
+let firstnumber = prompt ("Enter First Number")
+let operation = prompt ("Write Operation")
+let secondnumber = prompt ("Enter Second Number")
